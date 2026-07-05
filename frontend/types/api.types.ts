a982ba46f0149b34cddacda5b7a50e211432d887
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2026-07-05 17:08:19 +0200

    feat: add API types
    Resolves #47

---
+++ frontend/types/api.types.ts
@@ -0,0 +1,108 @@
+export type ErrorDetail = {
+    field?: string[],
+    message?: string[],
+};
+
+export type ErrorResponse = {
+    success: false,
+    message: string,
+    error: string,
+    details?: ErrorDetail[],
+};
+
+export type Success<T> = {
+    success: true,
+    message: string,
+    data: T,
+};
+
+export type ProjectRole = "OWNER" | "ADMIN" | "CONTRIBUTOR";
+
+export type ProjectMember = {
+    id: string,
+    role: ProjectRole,
+    user: User,
+
+    joinedAt: string,
+};
+
+export type Project = {
+    id: string,
+    name: string,
+
+    description: string,
+    ownerId: string,
+    owner: User,
+    members: ProjectMember[],
+
+    createdAt: string,
+    updatedAt: string,
+};
+
+export type TaskStatus = "TODO" | "IN_PROGRESS" | "DONE" | "CANCELLED";
+export type TaskPriority = "LOW" | "MEDIUM" | "HIGH" | "URGENT";
+
+export type TaskAssignee = {
+    id: string,
+    
+    userId: string,
+    taskId: string,
+    
+    user: User,
+    
+    assignedAt: string,
+};
+
+export type User = {
+    id: string,
+    
+    email: string,
+    name: string,
+    
+    createdAt: string,
+    updatedAt: string,
+};
+
+export type CommentAuthor = {
+    id: string,
+    
+    email: string,
+    name: string,
+    
+    createdAt: string,
+    updatedAt: string,
+};
+
+export type Comment = {
+    id: string,
+    
+    content: string,
+    taskId: string,
+    
+    authorId: string,
+    author: CommentAuthor,
+
+    createdAt: string,
+    updatedAt: string,
+};
+
+export type Task = {
+    id: string,
+
+    title: string,
+    description?: string,
+
+    status: TaskStatus,
+
+    priority: TaskPriority,
+    dueDate: string,
+
+    projectId: string,
+    creatorId: string,
+
+    assignees: TaskAssignee[],
+    comments: Comment[],
+
+    createdAt: string,
+    updatedAt: string,
+};
