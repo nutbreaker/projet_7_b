d0ff42f4228f0d9444a70b3ea499699a975ac4ee
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2026-08-22 23:23:05 +0200

    feat(not-found): improve layout

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="not-found">
       <h2>Not Found</h2>
-      <p>La ressource demandée n&rsquo;a pas été toruvée</p>
+      <p>La ressource demandée n&rsquo;a pas été trouvée</p>
       <Link href="/">Revenir à l&rsquo;accueil</Link>
     </div>
   );
