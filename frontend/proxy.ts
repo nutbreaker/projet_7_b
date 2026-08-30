15cb9b6acbca5341dbbb8258c47c51b229d3b69f
nutbreaker <10211888+nutbreaker@users.noreply.github.com>
2026-08-30 15:31:06 +0200

    fix(proxy): force user to have a valid cookie

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getSessionToken } from './services/session';
 import decodeJWT from './utils/jwt-decoder';
+import { authProfile } from './services/auth-profile';
 
 // https://nextjs.org/docs/pages/guides/authentication#optimistic-checks-with-proxy-optional
 
@@ -12,12 +13,13 @@ export default async function proxy(req: NextRequest) {
     const isPublicRoute = publicRoutes.includes(path);
     const cookie = await getSessionToken();
     const userInfo = await decodeJWT(cookie);
+    const userProfile = await authProfile(cookie as string);
 
-    if (!isPublicRoute && !userInfo?.userId) {
+    if (!isPublicRoute && (!userInfo?.userId || !userProfile.success)) {
         return NextResponse.redirect(new URL('/signin', req.nextUrl))
     }
 
-    if (userInfo?.userId && (isPublicRoute || path === '/')) {
+    if (userInfo?.userId && userProfile.success && (isPublicRoute || path === '/')) {
         return NextResponse.redirect(new URL('/dashboard', req.nextUrl))
     }
 
